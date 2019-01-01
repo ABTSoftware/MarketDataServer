0ad3528e534348dd2cda1ba17c37cb571e377ac3
Andrew Burnett-Thompson <andrew@scichart.com>
2019-01-01 23:23:49 +0000

    First commit

---
+++ src/scitrader.marketdataserver/src/main/java/Main.java
@@ -0,0 +1,7 @@
+package com.scitrader.marketdataserver;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("WOOT Zomgosh I am haxxing your computerr!"); // Display the string.
+  }
+}
