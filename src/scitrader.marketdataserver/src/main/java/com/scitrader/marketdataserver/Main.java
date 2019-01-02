3e58fc1e9e5436b9e840e1f02541a9c43679223d
Andrew Burnett-Thompson <andrew@scichart.com>
2019-01-02 00:02:55 +0000

    Added Log4J

--- src/scitrader.marketdataserver/src/main/java/com/scitrader/marketdataserver/Main.java
+++ src/scitrader.marketdataserver/src/main/java/com/scitrader/marketdataserver/Main.java
@@ -1,7 +1,16 @@
 package com.scitrader.marketdataserver;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+  private final static Logger log = LogManager.getLogger(Main.class);
+
   public static void main(String[] args) {
-    System.out.println("WOOT Zomgosh I am haxxing your computerr!"); // Display the string.
+    //Injector injector = Guice.createInjector(new BasicModule());
+    log.info("I am in your base, killing your doods");
   }
 }
