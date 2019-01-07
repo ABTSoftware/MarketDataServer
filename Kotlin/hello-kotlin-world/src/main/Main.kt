e5b499bf86f7d12cee6eeba2b46afa7c5e0aaae1
Andrew Burnett-Thompson <andrew@scichart.com>
2019-01-07 17:41:01 +0000

    Create Main.kt

---
+++ Kotlin/hello-kotlin-world/src/main/Main.kt
@@ -0,0 +1,19 @@
+package com.sandbox.helloworldkotlin
+
+//import com.google.inject.Guice
+//import com.google.inject.Injector
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+//private val log = LogManager.getLogger(Main::class.java)
+
+fun main(args: Array<String>) {
+
+    //log.info("I am in your base, killing your doods")
+
+    //val injector = Guice.createInjector(MarketDataServerModule())
+    //val server = injector.getInstance(MarketDataServer::class.java)
+
+    //server.Run()
+}
+
