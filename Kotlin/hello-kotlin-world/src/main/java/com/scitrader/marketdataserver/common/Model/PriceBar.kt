7bb1649efad43eb99ed47aefc3d3ddc4879abe8c
Andrew Burnett-Thompson <andrew@scichart.com>
2019-01-08 00:24:41 +0000

    attempting to fix serialiaton

--- Kotlin/hello-kotlin-world/src/main/java/com/scitrader/marketdataserver/common/Model/PriceBar.kt
+++ Kotlin/hello-kotlin-world/src/main/java/com/scitrader/marketdataserver/common/Model/PriceBar.kt
@@ -1,29 +1,25 @@
 package com.scitrader.marketdataserver.common.Model
 
+import com.jsoniter.annotation.JsonCreator
 import com.jsoniter.annotation.JsonProperty
 import org.joda.time.DateTime
 
 class PriceBar () {
 
-    @field:JsonProperty("time")
     var time : DateTime = DateTime(0)
-    @field:JsonProperty("open")
     var open : Double = 0.0
-    @field:JsonProperty("high")
     var high : Double = 0.0
-    @field:JsonProperty("low")
     var low : Double = 0.0
-    @field:JsonProperty("close")
     var close : Double = 0.0
-    @field:JsonProperty("volume")
     var volume : Double = 0.0
 
-    constructor(time: DateTime,
-                open : Double,
-                high : Double,
-                low : Double,
-                close : Double,
-                volume : Double) : this(){
+    @JsonCreator
+    constructor(@JsonProperty("time") time: DateTime,
+                @JsonProperty("open") open : Double,
+                @JsonProperty("high") high : Double,
+                @JsonProperty("low") low : Double,
+                @JsonProperty("close") close : Double,
+                @JsonProperty("volume") volume : Double) : this(){
         this.time = time
         this.open = open
         this.high = high
